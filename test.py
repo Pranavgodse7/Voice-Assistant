d16f04ca4d2ce49dfda307c7aed841d890c00c7a
Pranav <pranavgodse08@gmail.com>
2024-11-13 17:08:01 +0530

    initial commit
    initial commit

---
+++ test.py
@@ -0,0 +1,4 @@
+import os
+dictapp = {"commandprompt": "cmd", "paint": "MsPaint", "word": "winword", "excel": "excel", "chrome": "chrome", "vscode": "code", "powerpoint": "powerpnt","discord":"discord"}
+
+os.system(f"start {dictapp['discord']}")
